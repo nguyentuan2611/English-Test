a8e0e85e654a8a1cb1ab633830b3bfd06b7a13b8
nguyentuan2611 <nguyentuan261100@gmail.com>
2022-05-15 23:33:26 +0700

    up

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TrangchuComponent } from './pages/trangchu/trangchu.component';
+import { XephangComponent } from './pages/xephang/xephang.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+registerLocaleData(en);
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TrangchuComponent,
+    XephangComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FontAwesomeModule
+  ],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/pages/xephang/xephang.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-xephang',
+  templateUrl: './xephang.component.html',
+  styleUrls: ['./xephang.component.scss']
+})
+export class XephangComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
