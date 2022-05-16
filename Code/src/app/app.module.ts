14e313f12afbd3039e22683a033d3a392d2fed36
snoopdog226 <mallothi1@gmail.com>
2022-05-16 21:42:13 +0700

    them database, form dang nhap

--- Code/src/app/app.module.ts
+++ Code/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TrangchuComponent } from './pages/trangchu/trangchu.component';
 import { XephangComponent } from './pages/xephang/xephang.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { DangNhapComponent } from './pages/DangNhap/dang-nhap/dang-nhap.component';
 
 registerLocaleData(en);
 
@@ -21,7 +22,8 @@ registerLocaleData(en);
   declarations: [
     AppComponent,
     TrangchuComponent,
-    XephangComponent
+    XephangComponent,
+    DangNhapComponent
   ],
   imports: [
     BrowserModule,
