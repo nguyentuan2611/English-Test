6e10db2c5ba156afb63ac45f7e75eee3464b12d9
caubehaykhoc252 <caubehaykhocsn2000@gmail.com>
2022-05-27 14:12:21 +0700

    handle sign-up form

--- Code/src/app/layout/Dangky/dang-ky.component.ts
+++ Code/src/app/layout/Dangky/dang-ky.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { user } from 'src/app/shared/model/user.model';
+import { SignUpService } from 'src/app/shared/service/sign-up.service';
 
 @Component({
   selector: 'app-dangky',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dang-ky.component.scss']
 })
 export class DangkyComponent implements OnInit {
-
-  constructor() { }
+  public user: user = new user;
+  constructor(private signUpService: SignUpService , private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  signUp(){
+
+    this.signUpService.signUp(this.user).subscribe(res => {
+      let dataRes = res as {
+        resCode: string,
+        message: string,
+        data:{
+          userName: string,
+          fullName: string
+        }
+      }
+      if(dataRes.resCode == 'S001'){
+        alert(dataRes.message);
+        this.router.navigate(['/login']);
+      }
+      else if(dataRes.resCode == 'D001'){
+        alert(dataRes.message);
+      }
+    },err => console.log(err));
   }
 
 }
