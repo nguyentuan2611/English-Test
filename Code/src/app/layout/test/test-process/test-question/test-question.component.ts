870edf15a6d297380a5ec420548d12255dc4088f
nguyentuan2611 <nguyentuan261100@gmail.com>
2022-05-29 21:02:51 +0700

    updateTestAnswer

--- Code/src/app/layout/test/test-process/test-question/test-question.component.ts
+++ Code/src/app/layout/test/test-process/test-question/test-question.component.ts
@@ -15,6 +15,7 @@ export class TestQuestionComponent implements OnInit {
   questionIndex: number;
 
   questions: question[] = [];
+  answers: any[] = [];
 
   currentQuestion: question = new question;
 
@@ -37,7 +38,20 @@ export class TestQuestionComponent implements OnInit {
       !this.questionIndex ? this.questionIndex = 1 : {}
 
       this.currentQuestion = this.questions[this.questionIndex - 1]
+
+      if(this.answers.find( f => f.id == this.currentQuestion.id)){
+        var currentAnswer = this.answers.find( f => f.id == this.currentQuestion.id)
+        switch(currentAnswer.answer){
+          case this.currentQuestion.option1: this.onOptionChange(1); this.op1 = true; break
+          case this.currentQuestion.option2: this.onOptionChange(2); this.op2 = true; break
+          case this.currentQuestion.option3: this.onOptionChange(3); this.op3 = true; break
+          case this.currentQuestion.option4: this.onOptionChange(4); this.op4 = true; break
+        }
+      }else{
+        this.op1 = this.op2 = this.op3 = this.op4 = false
+      }
     })
+
   }
 
   getQuestion(){
@@ -45,12 +59,22 @@ export class TestQuestionComponent implements OnInit {
   }
 
   onOptionChange(num: any){
+
+    var answer = this.answers.find(f => f.id === this.currentQuestion.id)
+
+    this.answers = this.answers.filter(arr => arr !== answer)
+
     switch(num){
-      case 1: this.op2 = this.op3 = this.op4 = false; break
-      case 2: this.op1 = this.op3 = this.op4 = false; break
-      case 3: this.op1 = this.op2 = this.op4 = false; break
-      case 4: this.op1 = this.op2 = this.op3 = false; break
+      case 1: this.answers.push({ id: this.currentQuestion.id, answer: this.currentQuestion.option1})
+              this.op2 = this.op3 = this.op4 = false; break
+      case 2: this.answers.push({ id: this.currentQuestion.id, answer: this.currentQuestion.option2})
+              this.op1 = this.op3 = this.op4 = false; break
+      case 3: this.answers.push({ id: this.currentQuestion.id, answer: this.currentQuestion.option3})
+              this.op1 = this.op2 = this.op4 = false; break
+      case 4: this.answers.push({ id: this.currentQuestion.id, answer: this.currentQuestion.option4})
+              this.op1 = this.op2 = this.op3 = false; break
     }
+
   }
 
   getQuestions(){
--- Code/src/app/layout/test/test-routing.module.ts
+++ Code/src/app/layout/test/test-routing.module.ts
@@ -16,6 +16,7 @@ const routes: Routes = [
         { path: 'test-process', component: TestProcessComponent,
           children: [
             { path: '', redirectTo: 'test-question/',  },
+            { path: 'test-question', redirectTo: 'test-question/',  },
             { path: 'test-question/:index', component: TestQuestionComponent },
       ] },
     ]
